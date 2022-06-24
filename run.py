6e9086523399d5108677cf8d46b42bca65a9cb4c
ziimiin14 <erzimin@gmail.com>
2022-06-24 11:14:37 +0800

    Inference function/module done

--- run.py
+++ run.py
@@ -1,26 +1,181 @@
 import numpy as np
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModel, TrainingArguments
+import pandas as pd
 import os
 import torch
-import pandas as pd
-from datasets import load_metric,load_dataset
+import time
+import argparse
+import re
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import get_scheduler
+from datasets import load_metric,load_dataset,Dataset
 from sklearn.metrics import confusion_matrix, classification_report,roc_curve,auc
 from torch.utils.data import DataLoader
 from torch.optim import AdamW
-from transformers import get_scheduler
 from tqdm.auto import tqdm
-from collections import defaultdict
-import time
-import argparse
+from collections import defaultdict,deque
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
+def process(sentence,fil):
+  return fil.sub(' ',sentence)
+
+def tokenize_function(examples,tokenizer):
+    return tokenizer(examples["sentence"], padding="max_length", truncation=True)
 
 def inference():
-    pass
+    # Declare model/tokenizer/device variables
+    tokenizer_path = './tokenizer_last/'
+    model_path = './model_last/'
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    # Load Model and Tokenized
+    print('Loading Model and Tokenizer \n')
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+    ################################################################################################################################################################
+    # Declare variable for chromedriver
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome('chromedriver',options=options)
+
+    # Declare variable for selenium scraping
+    stock_codes={'AAPL':'(Apple|AAPL)','MSFT':'(Microsoft|MSFT)','AMZN':'(Amazon|AMZN)','META':'(Meta|META)','GOOGL':'(Alphabet|Google|GOOGL)','BRK.B':'(Berkshire|BRK.B)','TSLA':'(Tesla|TSLA)','NVDA':'(Nvidia|NVDA)','JPM':'(JPMorgan|JPM)'}
+    title = []
+    href = []
+    tc = []
+    mention = []
+
+    # Ask for user input for specific stock news from stock_code list
+    print(200*'-')
+    sc = input(f'Choose a stock: \n{list(stock_codes.keys())} \n-->')
+
+    # Scrape specific news title and href
+    print(200*'-')
+    print(f'-->Scraping News for {sc} stock<--\n')
+    url = f'https://www.cnbc.com/quotes/{sc}?tab=news'
+    driver.get(url)
+    try:
+      element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "LatestNews-newsTabButton")))
+      element.click()
+
+      element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "LatestNews-newsTabButton")))
+      element.click()
+    except:
+      print(f"Cannot find 2nd View More button for {sc}")
+
+    d = driver.page_source
+    soup = BeautifulSoup(d,'html.parser')
+    group = soup.find_all('div',attrs={'data-analytics':'QuotePage-QuoteNews-1'})
+    headlines = deque(group[0].find_all('a'))
+
+    final_headlines = []
+
+    while headlines:
+      temp = headlines.popleft()
+
+      if temp['class'][0] != 'LatestNews-headline':
+        headlines.popleft()
+      else:
+        if not temp.img:
+          final_headlines.append(temp)
+
+    for x in final_headlines:
+      tc.append(sc)
+      mention.append(stock_codes[sc])
+      title.append(x['title'])
+      href.append(x['href'])
+
+    df = pd.DataFrame({'Symbol':tc,'Mention':mention,'title':title,'link':href})
+
+    text_list = []
+
+    # Open each href link and read the keypoints text or body texts
+    for m,url_new in zip(df['Mention'],df['link']):
+      driver.get(url_new)
+      filter = re.compile(f'{m}')
+      soup_news = BeautifulSoup(driver.page_source,'html.parser')
+      keyPoint = soup_news.find_all('div',attrs={'data-test':'keyPoints-1'})
+
+      if not keyPoint:
+        body = soup_news.find_all('div',attrs={'data-module':'ArticleBody'})
+        group = body[0].find_all('div',attrs={'class':'group'})
+        paragraphs = []
+        for g in group:
+          p_temp = g.find_all('p')
+          for p in p_temp: 
+            paragraphs.append(p)
+
+        text = ''
+        count = 0
+
+        for p in paragraphs:
+          if filter.search(p.text):
+            text += p.text.strip()
+            count += 1
+          if count == 2:
+            break
+        if not text:
+          if len(paragraphs) > 1:
+            text = paragraphs[0].text.strip() + paragraphs[1].text.strip()
+          else:
+            text = paragraphs[0].text.strip()
+
+        text_list.append(text)
+
+      else:
+        li = keyPoint[0].find_all('li')
+        text = ''
+        for t in li:
+          text += t.text.strip()
+        text_list.append(text)
+
+    df['sentence'] = text_list
+    f = re.compile(f'\xa0')
+    df['sentence'] = df['sentence'].apply(process,args=(f,))
+    driver.quit()
+    print('-->Done<--\n')
+
+
+    ################################################################################################################################################
+    # Setup tokenized dataset
+    datasets = Dataset.from_dict({'sentence':df['sentence'].tolist()})
+    tokenized_datasets = datasets.map(tokenize_function,batched=True,fn_kwargs={'tokenizer':tokenizer})
+    tokenized_datasets = tokenized_datasets.remove_columns(['sentence'])
+    # tokenized_sentence = tokenizer(df['sentence'].tolist(),padding="max_length", truncation=True)
+    # tokenized_datasets = Dataset.from_dict(tokenized_sentence)
+    tokenized_datasets.set_format("torch")
+
+    # Dataloader
+    data_loader = DataLoader(tokenized_datasets,batch_size=tokenized_datasets.num_rows)
+
+    # Predict sentiment for the news
+    model.to(device)
+    model.eval()
+    print(200*'-')
+    print('-->Predicting sentiment for news<--\n')
+    for batch in data_loader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+        logits = outputs.logits
+        predictions = torch.argmax(logits, dim=-1)
+
+
+    df['sentiment'] = predictions.cpu().detach().numpy()
+    print('-->Done<--\n')
+    print(df)
+
 
 
 def eval():
@@ -38,11 +193,8 @@ def eval():
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
     model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
-    def tokenize_function(examples):
-        return tokenizer(examples["sentence"], padding="max_length", truncation=True)
-
     # Setup tokenized dataset
-    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    tokenized_datasets = dataset.map(tokenize_function, batched=True,fn_kwargs={'tokenizer':tokenizer})
     tokenized_datasets = tokenized_datasets.remove_columns(["sentence"])
     tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
     tokenized_datasets.set_format("torch")
@@ -109,11 +261,8 @@ def train():
     tokenizer = AutoTokenizer.from_pretrained(model_name,do_lower_case=True,model_max_length=256)
     model = AutoModelForSequenceClassification.from_pretrained(model_name,num_labels=3)
 
-    def tokenize_function(examples):
-        return tokenizer(examples["sentence"], padding="max_length", truncation=True)
-
     # Setup tokenized dataset
-    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    tokenized_datasets = dataset.map(tokenize_function, batched=True,fn_kwargs={'tokenizer':tokenizer})
     tokenized_datasets = tokenized_datasets.remove_columns(["sentence"])
     tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
     tokenized_datasets.set_format("torch")
